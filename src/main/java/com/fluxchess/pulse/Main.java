42ea67a406856dea60ba1c9ca14779bb62f8ffa1
Phokham Nonava <pn@nonava.com>
2014-09-05 22:57:41 +0200

    Add Main class as entry point

---
+++ src/main/java/com/fluxchess/pulse/Main.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2013-2014 Phokham Nonava
+ *
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+package com.fluxchess.pulse;
+
+public final class Main {
+
+  public static void main(String[] args) {
+    // Don't do any fancy stuff here. Just create our engine and
+    // run it. JCPI takes care of the rest. It waits for the GUI
+    // to issue commands which will call our methods using the
+    // visitor pattern.
+    try {
+      if (args.length == 1) {
+        if (args[0].equalsIgnoreCase("perft")) {
+          new Perft().run();
+        }
+      } else {
+        new Pulse().run();
+      }
+    } catch (Throwable t) {
+      System.out.format("Exiting Pulse due to an exception: %s%n", t.getLocalizedMessage());
+      t.printStackTrace();
+      System.exit(1);
+    }
+  }
+
+}
--- src/main/java/com/fluxchess/pulse/Pulse.java
+++ src/main/java/com/fluxchess/pulse/Pulse.java
@@ -21,31 +21,11 @@ import java.io.PrintStream;
  * UCI protocol. We simply extend AbstractEngine and implement the required
  * methods.
  */
-public final class Pulse extends AbstractEngine {
+final class Pulse extends AbstractEngine {
 
   // We have to maintain at least the state of the board and the search.
   private Board board = new Board(new GenericBoard(GenericBoard.STANDARDSETUP));
   private Search search = Search.newInfiniteSearch(getProtocol(), board);
-
-  public static void main(String[] args) {
-    // Don't do any fancy stuff here. Just create our engine and
-    // run it. JCPI takes care of the rest. It waits for the GUI
-    // to issue commands which will call our methods using the
-    // visitor pattern.
-    try {
-      if (args.length == 1) {
-        if (args[0].equalsIgnoreCase("perft")) {
-          new Perft().run();
-        }
-      } else {
-        new Pulse().run();
-      }
-    } catch (Throwable t) {
-      System.out.format("Exiting Pulse due to an exception: %s%n", t.getLocalizedMessage());
-      t.printStackTrace();
-      System.exit(1);
-    }
-  }
 
   // AbstractEngine provides three constructors to help us connecting to a
   // command channel.
