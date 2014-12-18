2cf3090162f2a7b8da45fd53b013a9f6bc40ea31
Phokham Nonava <pn@nonava.com>
2014-12-18 21:09:44 +0100

    PULSE-64: Add static import in PositionTest

--- src/test/java/com/fluxchess/pulse/PositionTest.java
+++ src/test/java/com/fluxchess/pulse/PositionTest.java
@@ -8,6 +8,44 @@ package com.fluxchess.pulse;
 
 import org.junit.Test;
 
+import static com.fluxchess.pulse.Castling.NOCASTLING;
+import static com.fluxchess.pulse.Castling.WHITE_KINGSIDE;
+import static com.fluxchess.pulse.Castling.WHITE_QUEENSIDE;
+import static com.fluxchess.pulse.Color.BLACK;
+import static com.fluxchess.pulse.Color.WHITE;
+import static com.fluxchess.pulse.MoveType.CASTLING;
+import static com.fluxchess.pulse.MoveType.ENPASSANT;
+import static com.fluxchess.pulse.MoveType.NORMAL;
+import static com.fluxchess.pulse.MoveType.PAWNDOUBLE;
+import static com.fluxchess.pulse.MoveType.PAWNPROMOTION;
+import static com.fluxchess.pulse.Piece.BLACK_KNIGHT;
+import static com.fluxchess.pulse.Piece.BLACK_PAWN;
+import static com.fluxchess.pulse.Piece.NOPIECE;
+import static com.fluxchess.pulse.Piece.WHITE_KING;
+import static com.fluxchess.pulse.Piece.WHITE_KNIGHT;
+import static com.fluxchess.pulse.Piece.WHITE_PAWN;
+import static com.fluxchess.pulse.Piece.WHITE_QUEEN;
+import static com.fluxchess.pulse.PieceType.NOPIECETYPE;
+import static com.fluxchess.pulse.PieceType.QUEEN;
+import static com.fluxchess.pulse.Square.NOSQUARE;
+import static com.fluxchess.pulse.Square.a2;
+import static com.fluxchess.pulse.Square.a3;
+import static com.fluxchess.pulse.Square.a4;
+import static com.fluxchess.pulse.Square.a7;
+import static com.fluxchess.pulse.Square.a8;
+import static com.fluxchess.pulse.Square.b1;
+import static com.fluxchess.pulse.Square.b6;
+import static com.fluxchess.pulse.Square.b7;
+import static com.fluxchess.pulse.Square.b8;
+import static com.fluxchess.pulse.Square.c1;
+import static com.fluxchess.pulse.Square.c3;
+import static com.fluxchess.pulse.Square.c6;
+import static com.fluxchess.pulse.Square.d3;
+import static com.fluxchess.pulse.Square.d4;
+import static com.fluxchess.pulse.Square.e1;
+import static com.fluxchess.pulse.Square.e4;
+import static com.fluxchess.pulse.Square.f3;
+import static com.fluxchess.pulse.Square.g1;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -18,14 +56,14 @@ public class PositionTest {
     Position position = Notation.toPosition(Notation.STANDARDPOSITION);
 
     // Move white pawn
-    int move = Move.valueOf(MoveType.NORMAL, Square.a2, Square.a3, Piece.WHITE_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(NORMAL, a2, a3, WHITE_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
-    assertThat(position.activeColor, is(Color.BLACK));
+    assertThat(position.activeColor, is(BLACK));
 
     // Move black pawn
-    move = Move.valueOf(MoveType.NORMAL, Square.b7, Square.b6, Piece.BLACK_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, b7, b6, BLACK_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
-    assertThat(position.activeColor, is(Color.WHITE));
+    assertThat(position.activeColor, is(WHITE));
   }
 
   @Test
@@ -33,16 +71,16 @@ public class PositionTest {
     Position position = Notation.toPosition(Notation.STANDARDPOSITION);
 
     // Move white pawn
-    int move = Move.valueOf(MoveType.NORMAL, Square.a2, Square.a3, Piece.WHITE_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(NORMAL, a2, a3, WHITE_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
     assertThat(position.halfmoveClock, is(0));
 
     // Move black pawn
-    move = Move.valueOf(MoveType.NORMAL, Square.b7, Square.b6, Piece.BLACK_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, b7, b6, BLACK_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
     // Move white knight
-    move = Move.valueOf(MoveType.NORMAL, Square.b1, Square.c3, Piece.WHITE_KNIGHT, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, b1, c3, WHITE_KNIGHT, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
     assertThat(position.halfmoveClock, is(1));
   }
@@ -52,12 +90,12 @@ public class PositionTest {
     Position position = Notation.toPosition(Notation.STANDARDPOSITION);
 
     // Move white pawn
-    int move = Move.valueOf(MoveType.NORMAL, Square.a2, Square.a3, Piece.WHITE_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(NORMAL, a2, a3, WHITE_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
     assertThat(position.getFullmoveNumber(), is(1));
 
     // Move black pawn
-    move = Move.valueOf(MoveType.NORMAL, Square.b7, Square.b6, Piece.BLACK_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, b7, b6, BLACK_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
     assertThat(position.getFullmoveNumber(), is(2));
   }
@@ -67,23 +105,23 @@ public class PositionTest {
     Position position = Notation.toPosition(Notation.STANDARDPOSITION);
 
     // Move white knight
-    int move = Move.valueOf(MoveType.NORMAL, Square.b1, Square.c3, Piece.WHITE_KNIGHT, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(NORMAL, b1, c3, WHITE_KNIGHT, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
     // Move black knight
-    move = Move.valueOf(MoveType.NORMAL, Square.b8, Square.c6, Piece.BLACK_KNIGHT, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, b8, c6, BLACK_KNIGHT, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
     // Move white knight
-    move = Move.valueOf(MoveType.NORMAL, Square.g1, Square.f3, Piece.WHITE_KNIGHT, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, g1, f3, WHITE_KNIGHT, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
     // Move black knight
-    move = Move.valueOf(MoveType.NORMAL, Square.c6, Square.b8, Piece.BLACK_KNIGHT, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, c6, b8, BLACK_KNIGHT, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
     // Move white knight
-    move = Move.valueOf(MoveType.NORMAL, Square.f3, Square.g1, Piece.WHITE_KNIGHT, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(NORMAL, f3, g1, WHITE_KNIGHT, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
     assertThat(position.isRepetition(), is(true));
@@ -106,7 +144,7 @@ public class PositionTest {
     Position position = Notation.toPosition(Notation.STANDARDPOSITION);
     long zobristKey = position.zobristKey;
 
-    int move = Move.valueOf(MoveType.NORMAL, Square.a2, Square.a3, Piece.WHITE_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(NORMAL, a2, a3, WHITE_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
     position.undoMove(move);
 
@@ -119,10 +157,10 @@ public class PositionTest {
     Position position = Notation.toPosition(Notation.STANDARDPOSITION);
     long zobristKey = position.zobristKey;
 
-    int move = Move.valueOf(MoveType.PAWNDOUBLE, Square.a2, Square.a4, Piece.WHITE_PAWN, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(PAWNDOUBLE, a2, a4, WHITE_PAWN, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
-    assertThat(position.enPassantSquare, is(Square.a3));
+    assertThat(position.enPassantSquare, is(a3));
 
     position.undoMove(move);
 
@@ -135,10 +173,10 @@ public class PositionTest {
     Position position = Notation.toPosition("8/P5k1/8/8/2K5/8/8/8 w - - 0 1");
     long zobristKey = position.zobristKey;
 
-    int move = Move.valueOf(MoveType.PAWNPROMOTION, Square.a7, Square.a8, Piece.WHITE_PAWN, Piece.NOPIECE, PieceType.QUEEN);
+    int move = Move.valueOf(PAWNPROMOTION, a7, a8, WHITE_PAWN, NOPIECE, QUEEN);
     position.makeMove(move);
 
-    assertThat(position.board[Square.a8], is(Piece.WHITE_QUEEN));
+    assertThat(position.board[a8], is(WHITE_QUEEN));
 
     position.undoMove(move);
 
@@ -152,12 +190,12 @@ public class PositionTest {
     long zobristKey = position.zobristKey;
 
     // Make en passant move
-    int move = Move.valueOf(MoveType.ENPASSANT, Square.e4, Square.d3, Piece.BLACK_PAWN, Piece.WHITE_PAWN, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(ENPASSANT, e4, d3, BLACK_PAWN, WHITE_PAWN, NOPIECETYPE);
     position.makeMove(move);
 
-    assertThat(position.board[Square.d4], is(Piece.NOPIECE));
-    assertThat(position.board[Square.d3], is(Piece.BLACK_PAWN));
-    assertThat(position.enPassantSquare, is(Square.NOSQUARE));
+    assertThat(position.board[d4], is(NOPIECE));
+    assertThat(position.board[d3], is(BLACK_PAWN));
+    assertThat(position.enPassantSquare, is(NOSQUARE));
 
     position.undoMove(move);
 
@@ -170,10 +208,10 @@ public class PositionTest {
     Position position = Notation.toPosition("r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1");
     long zobristKey = position.zobristKey;
 
-    int move = Move.valueOf(MoveType.CASTLING, Square.e1, Square.c1, Piece.WHITE_KING, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    int move = Move.valueOf(CASTLING, e1, c1, WHITE_KING, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
-    assertThat(position.castlingRights & Castling.WHITE_QUEENSIDE, is(Castling.NOCASTLING));
+    assertThat(position.castlingRights & WHITE_QUEENSIDE, is(NOCASTLING));
 
     position.undoMove(move);
 
@@ -183,10 +221,10 @@ public class PositionTest {
     position = Notation.toPosition("r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1");
     zobristKey = position.zobristKey;
 
-    move = Move.valueOf(MoveType.CASTLING, Square.e1, Square.g1, Piece.WHITE_KING, Piece.NOPIECE, PieceType.NOPIECETYPE);
+    move = Move.valueOf(CASTLING, e1, g1, WHITE_KING, NOPIECE, NOPIECETYPE);
     position.makeMove(move);
 
-    assertThat(position.castlingRights & Castling.WHITE_KINGSIDE, is(Castling.NOCASTLING));
+    assertThat(position.castlingRights & WHITE_KINGSIDE, is(NOCASTLING));
 
     position.undoMove(move);
 
