03f9cfabff9823b06e00a47c47f80b1b664f5d7a
Phokham Nonava <pn@nonava.com>
2017-07-05 22:51:39 +0200

    PULSE-81: Refactor main method

--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -10,20 +10,24 @@
 
 #include <iostream>
 
+int printUsage() {
+	std::cout << "Usage: pulse [perft]" << std::endl;
+	return 1;
+}
+
 int main(int argc, char* argv[]) {
-	try {
-		if (argc == 2) {
-			std::string token(argv[1]);
-			if (token == "perft") {
-				std::unique_ptr<pulse::Perft> perft(new pulse::Perft());
-				perft->run();
-			}
+	if (argc == 1) {
+		std::unique_ptr<pulse::Pulse> pulse(new pulse::Pulse());
+		pulse->run();
+	} else if (argc == 2) {
+		std::string token(argv[1]);
+		if (token == "perft") {
+			std::unique_ptr<pulse::Perft> perft(new pulse::Perft());
+			perft->run();
 		} else {
-			std::unique_ptr<pulse::Pulse> pulse(new pulse::Pulse());
-			pulse->run();
+			return printUsage();
 		}
-	} catch (std::exception& e) {
-		std::cout << "Exiting Pulse due to an exception: " << e.what() << std::endl;
-		return 1;
+	} else {
+		return printUsage();
 	}
 }
