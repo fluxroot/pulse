d654165665b6790c27a46be26cf6741459a1b2d7
Phokham Nonava <pn@nonava.com>
2021-11-20 00:08:04 +0100

    Update copyright header for Java

--- src/test/java/com/fluxchess/pulse/MoveListTest.java
+++ src/test/java/com/fluxchess/pulse/MoveListTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2019 Phokham Nonava
+ * Copyright 2013-2021 Phokham Nonava
  *
  * Use of this source code is governed by the MIT license that can be
  * found in the LICENSE file.
