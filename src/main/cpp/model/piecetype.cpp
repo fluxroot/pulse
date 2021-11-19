c5b106ce8baaca3c85e4bb3235cd6e4f73baf2b3
Phokham Nonava <pn@nonava.com>
2021-11-20 00:06:46 +0100

    Update copyright header for C++

--- src/main/cpp/model/piecetype.cpp
+++ src/main/cpp/model/piecetype.cpp
@@ -1,9 +1,7 @@
-/*
- * Copyright (C) 2013-2019 Phokham Nonava
- *
- * Use of this source code is governed by the MIT license that can be
- * found in the LICENSE file.
- */
+// Copyright 2013-2021 Phokham Nonava
+//
+// Use of this source code is governed by the MIT license that can be
+// found in the LICENSE file.
 
 #include <exception>
 #include "piecetype.h"
