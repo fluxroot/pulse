ef509bd1d2eaab482a8cdab9e4140621930b56e7
Phokham Nonava <pn@nonava.com>
2023-03-13 22:52:25 +0100

    Add Pulse Go main

---
+++ pulse-go/cmd/pulse.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2013-2023 Phokham Nonava
+ *
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package main
+
+func main() {
+}
