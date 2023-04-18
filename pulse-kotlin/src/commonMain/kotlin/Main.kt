16e4e551549f9b36e0526905976a9af1debdb939
Phokham Nonava <pn@nonava.com>
2023-04-18 12:42:19 +0200

    Move copyright to top of file

--- pulse-kotlin/src/commonMain/kotlin/Main.kt
+++ pulse-kotlin/src/commonMain/kotlin/Main.kt
@@ -1,13 +1,13 @@
-import com.fluxchess.pulse.kotlin.Perft
-import com.fluxchess.pulse.kotlin.Pulse
-import kotlin.system.exitProcess
-
 /*
  * Copyright 2013-2023 Phokham Nonava
  *
  * Use of this source code is governed by the MIT license that can be
  * found in the LICENSE file.
  */
+
+import com.fluxchess.pulse.kotlin.Perft
+import com.fluxchess.pulse.kotlin.Pulse
+import kotlin.system.exitProcess
 
 fun main(args: Array<String>) = when {
 	args.isEmpty() -> {
